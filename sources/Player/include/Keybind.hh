3040eeca6fa32aeff92457c255d646a9160ba538
jules palluau <jules.palluau@epitech.eu>
2016-05-10 11:56:03 +0200

    ubuntu

--- sources/Player/include/Keybind.hh
+++ sources/Player/include/Keybind.hh
@@ -5,7 +5,7 @@
 // Login   <pallua_j@epitech.eu>
 //
 // Started on  Tue May 10 10:27:51 2016 Jules Palluau
-// Last update Tue May 10 10:37:50 2016 Jules Palluau
+// Last update Tue May 10 11:23:22 2016 Jules Palluau
 //
 
 #ifndef _KEYBIND_HH_
@@ -18,13 +18,16 @@
 class Keybind
 {
 private:
-  std::map<e_action, int>  bind;
+  std::map<int, e_action>  bind1;
+  std::map<int, e_action>  bind2;
 public:
   Keybind();
   Keybind(const Keybind &);
   ~Keybind();
   const Keybind &operator=(const Keybind &);
   void  init(const std::string &);
+  void  override();
+  const e_action  &get_action(const int &, const int &);
 };
 
 #endif /*!_KEYBIND_HH_*/
--- sources/Player/src/Keybind.cpp
+++ sources/Player/src/Keybind.cpp
@@ -5,19 +5,30 @@
 // Login   <pallua_j@epitech.eu>
 //
 // Started on  Tue May 10 10:32:14 2016 Jules Palluau
-// Last update Tue May 10 10:38:31 2016 Jules Palluau
+// Last update Tue May 10 11:23:36 2016 Jules Palluau
 //
 
 #include "../include/Keybind.hh"
 
 Keybind::Keybind()
 {
-  
+  bind1[0x5A] = UP;
+  bind1[0x53] = DOWN;
+  bind1[0x51] = LEFT;
+  bind1[0x44] = RIGHT;
+  bind1[0x20] = BOMB;
+
+  bind2[0x26] = UP;
+  bind2[0x28] = DOWN;
+  bind2[0x25] = LEFT;
+  bind2[0x27] = RIGHT;
+  bind2[0xA1] = BOMB;
 }
 
 Keybind::Keybind(const Keybind &key)
 {
-  this->bind = key.bind;
+  this->bind1 = key.bind1;
+  this->bind2 = key.bind2;
 }
 
 Keybind::~Keybind()
@@ -27,11 +38,25 @@ Keybind::~Keybind()
 
 const Keybind   &Keybind::operator=(const Keybind &key)
 {
-  this->bind = key.bind;
+  this->bind2 = key.bind2;
+  this->bind1 = key.bind1;
   return (*this);
 }
 
 void  Keybind::init(const std::string &file)
 {
+  //TODO Liege
+}
 
+void  Keybind::override()
+{
+  //TODO Liege
+}
+
+const e_action  &Keybind::get_action(const int &key, const int &player)
+{
+  if (player == 1)
+    return (this->bind1[key]);
+  else
+    return (this->bind2[key]);
 }
