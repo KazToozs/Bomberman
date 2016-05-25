e9e704f34b96c7c69f1e44bd8834e263df72a36e
Quentin Fernandez <quentin.fernandez@epitech.eu>
2016-05-25 09:27:34 +0200

    Hey c'est reparer

--- sources/Menu/PlayerButton.cpp
+++ sources/Menu/PlayerButton.cpp
@@ -1,9 +1,9 @@
 //
 // SoloButton.cpp for  in /home/liege_m/rendu/cpp_indie_studio/sources/Menu
-// 
+//
 // Made by maxime liege
 // Login   <liege_m@epitech.net>
-// 
+//
 // Started on  Thu Apr 28 14:12:49 2016 maxime liege
 // Last update Wed May 18 13:29:47 2016 maxime liege
 //
@@ -12,41 +12,26 @@
 
 #include "IAButton.hh"
 
-PlayerButton::PlayerButton(const std::string &name, 
-			   const PlayerButton::MODE_PLAYER &id, Menu *menu)
-  : _id(id), _name(name)
-{
+PlayerButton::PlayerButton(const std::string &name,
+                           const PlayerButton::MODE_PLAYER &id, Menu *menu)
+    : _id(id), _name(name) {
   this->menu = menu;
 }
 
-PlayerButton::~PlayerButton()
-{
+PlayerButton::~PlayerButton() {}
 
-}
-
-void				PlayerButton::action() const
-{
+void PlayerButton::action() const {
   this->menu->clearList();
-  this->menu->pushBackList(new IAButton("IA1", IAButton::IA_1, this->menu, 
-					true));
-  this->menu->pushBackList(new IAButton("IA2", IAButton::IA_2, this->menu,
-					true));
-  if (this->_id == SOLO)
-    this->menu->pushBackList(new IAButton("IA3", IAButton::IA_3, this->menu,
-					  false));
+  this->menu->pushBackList(
+      new IAButton("IA1", IAButton::IA_1, this->menu, this->_id != SOLO));
+  this->menu->pushBackList(
+      new IAButton("IA2", IAButton::IA_2, this->menu, this->_id != SOLO));
+  this->menu->pushBackList(
+      new IAButton("IA3", IAButton::IA_3, this->menu, this->_id != SOLO));
 }
 
-bool				PlayerButton::getActive() const
-{
-  return this->isActive;
-}
+bool PlayerButton::getActive() const { return this->isActive; }
 
-const std::string		&PlayerButton::getName() const
-{
-  return this->_name;
-}
+const std::string &PlayerButton::getName() const { return this->_name; }
 
-PlayerButton::MODE_PLAYER	PlayerButton::getId() const
-{
-  return this->_id;
-}
+PlayerButton::MODE_PLAYER PlayerButton::getId() const { return this->_id; }
