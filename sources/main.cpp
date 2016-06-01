46790d124744fa36aa49bc4e5fd8dac565ee419c
Quentin Fernandez <quentin.fernandez@epitech.eu>
2016-06-01 16:18:34 +0200

    Hey le last switch case tu degage !

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,47 +6,45 @@
 #include "Menu.hh"
 
 int check_keys(Gui &g) {
-  static int ret = 0;
-  if (g.get_key_event()[irr::KEY_UP].PressedDown) {
-    if (ret == 1) std::this_thread::sleep_for(std::chrono::microseconds(100000));
-    ret = 1;
-    if (g.get_key_event()[irr::KEY_UP].PressedDown) return (1);
-  }
-  if (g.get_key_event()[irr::KEY_DOWN].PressedDown) {
-    if (ret == 2) std::this_thread::sleep_for(std::chrono::microseconds(100000));
-    ret = 2;
-    if (g.get_key_event()[irr::KEY_DOWN].PressedDown) return (2);
-  }
-  if (g.get_key_event()[irr::KEY_RETURN].PressedDown) {
-    if (ret == 3) std::this_thread::sleep_for(std::chrono::microseconds(100000));
-    ret = 3;
-    if (g.get_key_event()[irr::KEY_RETURN].PressedDown) return (3);
-  }
-  ret = 0;
-  return (0);
+	static int ret = 0;
+	if (g.get_key_event()[irr::KEY_UP].PressedDown) {
+		if (ret == 1) std::this_thread::sleep_for(std::chrono::microseconds(100000));
+		ret = 1;
+		if (g.get_key_event()[irr::KEY_UP].PressedDown) return (1);
+	}
+	if (g.get_key_event()[irr::KEY_DOWN].PressedDown) {
+		if (ret == 2) std::this_thread::sleep_for(std::chrono::microseconds(100000));
+		ret = 2;
+		if (g.get_key_event()[irr::KEY_DOWN].PressedDown) return (2);
+	}
+	if (g.get_key_event()[irr::KEY_RETURN].PressedDown) {
+		if (ret == 3) std::this_thread::sleep_for(std::chrono::microseconds(100000));
+		ret = 3;
+		if (g.get_key_event()[irr::KEY_RETURN].PressedDown) return (3);
+	}
+	ret = 0;
+	return (0);
 }
 
+Func_Ptr MenuBtnTab[] = {
+	&Menu::GoUp,
+	&Menu::GoDown,
+	&Menu::Action,
+	NULL
+};
+
 int main() {
-  Gui g;
-  Menu *menu = new Menu(&g);
-  int key;
-  g.Start(menu);
-  bool start = false;
+	Gui g;
+	Menu *menu = new Menu(&g);
+	int key;
+	g.Start(menu);
+	bool start = false;
 
-  while (g.Alive()) {
-    key = ((g.getGame() == NULL) ? check_keys(g) : 0);
-    switch (key) {
-      case 1:
-        menu->GoUp();
-        break;
-      case 2:
-        menu->GoDown();
-        break;
-      case 3:
-        menu->Action();
-        break;
-    }
-  }
+	while (g.Alive()) {
+		key = ((g.getGame() == NULL) ? check_keys(g) : 0);
+		if (key)
+			(menu->*MenuBtnTab[key - 1])();
+	}
 
-  return 0;
+	return 0;
 }
